7aaa39b3ee29bcec4e4c9efa62378bc67f399515
Brendan Wood <brendan.wood@concordia.ca>
2021-04-06 20:18:34 -0400

    Initial commit

---
+++ src/ca/concordia/id24178556/helloworldapp/Main.java
@@ -0,0 +1,9 @@
+package ca.concordia.id24178556.helloworldapp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+        System.out.println("WORLD");
+    }
+}
