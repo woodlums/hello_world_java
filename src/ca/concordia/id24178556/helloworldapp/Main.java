c0bcf9f58a5540c0b9b1c475baaa10ebd1916efc
Brendan Wood <brendan.wood@concordia.ca>
2021-04-06 20:51:24 -0400

    End of class 1

--- src/ca/concordia/id24178556/helloworldapp/Main.java
+++ src/ca/concordia/id24178556/helloworldapp/Main.java
@@ -3,7 +3,29 @@ package ca.concordia.id24178556.helloworldapp;
 public class Main {
 
     public static void main(String[] args) {
+
+        /*
+        This is a block level comment
+        and every line between the markers
+        is considered a comment.
+        */
+
+        // This calculates the amount owing to the bank.
+
+        // numbers
+        // strings
+
+        String first = "Brendan";
+
+        String last;
+
+        last = "Wood";
+
+
         System.out.println("HELLO");
-        System.out.println("WORLD");
+        System.out.println("WORLD"); // This line is interesting.
+
+        System.out.println(first + " " + last);
+
     }
 }
